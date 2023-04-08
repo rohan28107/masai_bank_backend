87b985b1cc6457e1c1efd0bd0910d349be24e8d0
rohan28107 <rohangajre21@gmail.com>
2023-04-08 15:00:59 +0530

    M14 backend 7th commit

--- models/Account.model.js
+++ models/Account.model.js
@@ -10,7 +10,7 @@ const accountSchema = new mongoose.Schema({
         required: true,
     },
     dob: {
-        type: String,
+        type: [Date],
         required: true,
     },
     email: {
